15af09b5a3c7221cd8b122913bc481074c2373af
c20liu <33676760+c20liu@users.noreply.github.com>
2018-06-19 21:51:29 +0800

    第一课作业

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,57 @@
 /*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract MyPayroll {
+    uint salary = 1 ether;
+    address employee;
+    address employer;
+    uint constant payDuration = 10 seconds;
+    uint lastPaydata = now;
+
+    function MyPayroll() {
+        employer = msg.sender;
+    }
+
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+
+    function calcutateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+
+    function hasEnougFund() returns (bool) {
+        return calcutateRunway() > 0;
+    }
+
+    function updateEmployee(address e) {
+        if (msg.sender != employer) {
+            revert();
+        }
+
+        employee = e;
+        lastPaydata = now;
+    }
+
+    function updateSalary(uint s) {
+        if (msg.sender != employer) {
+            revert();
+        }
+
+        salary = s;
+        lastPaydata = now;
+    }
+
+    function getPaid() {
+        if (msg.sender != employee) {
+            revert();
+        }
+        uint nextPaydata = lastPaydata + payDuration;
+        if (nextPaydata > now) {
+            revert();
+        }
+
+        lastPaydata = nextPaydata;
+        employee.transfer(salary);
+    }
+}
