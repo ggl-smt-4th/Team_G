c45722b4b5eaf884e376795b9ad67e903ba15ac0
forest21000 <93881254@qq.com>
2018-06-26 23:11:32 +0800

    Create Payroll.sol

---
+++ projects/lesson-3/contracts/Payroll.sol
@@ -0,0 +1,144 @@
+pragma solidity ^0.4.14;
+
+import './SafeMath.sol';
+import './Ownable.sol';
+
+contract Payroll is Ownable{
+    
+    using SafeMath for uint;
+    
+    struct struEmployee{
+        address addrOfEmployee;
+        uint salary;
+        uint lastPayday;
+    }
+    
+    uint constant payDuration = 10 seconds;
+
+    
+    mapping(address => struEmployee) public mapEmployees ;
+    
+    uint public ntotalSalary = 0;
+    
+    uint public nEmployeeNum = 0;
+    
+    modifier employeeExists(address addrOfEmployee) {
+        var employee = mapEmployees[addrOfEmployee];
+        assert(employee.addrOfEmployee != 0x0); 
+        _;
+    }
+    
+    modifier employeeChangeAddr(address oldAddress,address newAddress) {
+        require(oldAddress != newAddress);
+        
+        var employee = mapEmployees[oldAddress];
+        assert(employee.addrOfEmployee != 0x0);
+        _;
+    }
+    
+    //
+    function partialPaid(struEmployee employee) private{
+        
+        var timeInverval = now.sub(employee.lastPayday);
+        var salarySum = employee.salary.mul(timeInverval);
+        
+        uint partialdPaid = salarySum.div(payDuration);
+        employee.addrOfEmployee.transfer(partialdPaid);
+    }
+    
+    
+    function addFund() payable public returns(uint) {
+        
+        return address(this).balance;
+    }
+    
+    function getFund() public returns(uint) {
+        
+        return address(this).balance;
+    }
+    
+    //
+    function addEmployee(address addrOfEmployee,uint salary) public  onlyOwner{
+        
+        var employee = mapEmployees[addrOfEmployee];
+        assert(employee.addrOfEmployee == 0x0); 
+
+        ntotalSalary = ntotalSalary.add(salary * 1 ether);
+        
+        mapEmployees[addrOfEmployee]= struEmployee(addrOfEmployee,salary * 1 ether,now) ;
+        
+        nEmployeeNum = nEmployeeNum.add(1);
+        
+    }
+    
+    function removeEmployee(address addrOfEmployee) public onlyOwner employeeExists(addrOfEmployee) {
+
+        var employee = mapEmployees[addrOfEmployee];
+        
+        partialPaid(employee);
+        
+        ntotalSalary = ntotalSalary.sub(employee.salary);
+        
+        delete mapEmployees[employee.addrOfEmployee];
+        nEmployeeNum = nEmployeeNum.sub(1);
+    }
+    
+    function updateEmployee(address addrOfEmployee, uint salary) public onlyOwner employeeExists(addrOfEmployee){
+        
+        var employee = mapEmployees[addrOfEmployee];
+        
+        assert(employee.addrOfEmployee != 0x0);
+        
+        ntotalSalary = ntotalSalary.sub(employee.salary);
+        
+        partialPaid(employee);
+        employee.salary = salary * 1 ether;
+        employee.lastPayday = now;
+        
+        ntotalSalary = ntotalSalary.add(employee.salary);
+        
+    }
+    
+    function changePaymentAddress(address oldAddress, address newAddress) public onlyOwner employeeChangeAddr(oldAddress,newAddress){
+        
+        var employee = mapEmployees[oldAddress];
+        
+        partialPaid(employee);
+        
+        employee.addrOfEmployee = newAddress;
+        employee.lastPayday = now;
+        
+    }
+    
+    function checkEmployeeInfo(address addrOfEmployee) public employeeExists(addrOfEmployee) returns (uint salary,uint lastPayday) {
+        
+        var employee = mapEmployees[addrOfEmployee];
+
+        salary = employee.salary;
+        lastPayday = employee.lastPayday;
+    }
+    
+    function calculateRunway() public view returns (uint)  {
+            
+        assert(ntotalSalary!=0);
+        
+        return address(this).balance.div(ntotalSalary) ;
+    }
+    
+    function hasEnoughFund() public view returns (bool) {
+        
+        return calculateRunway()>0;
+    }
+    
+    function getPaid() public employeeExists(msg.sender){
+        
+        var employee = mapEmployees[msg.sender];
+        
+        uint nextPayday = employee.lastPayday.add(payDuration);
+        if(nextPayday >now) revert();
+
+        employee.lastPayday = nextPayday;
+        employee.addrOfEmployee.transfer(employee.salary);
+    }
+
+}
