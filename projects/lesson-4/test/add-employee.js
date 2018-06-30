11da1bbd923e1c87eaf9219c69fab87250a81ab0
forest21000 <93881254@qq.com>
2018-07-01 07:22:54 +0800

    Update add-employee.js

--- projects/lesson-4/test/add-employee.js
+++ projects/lesson-4/test/add-employee.js
@@ -1,11 +1,11 @@
 var Payroll = artifacts.require("./Payroll.sol");
 
 contract('Payroll', function (accounts) {
-  const owner = accounts[0];
-  const employee = accounts[1];
-  const employee_null = 0x0;
-  const guest = accounts[5];
-  const salary = 1;
+  const owner = accounts[0];  //当前账号的第1个地址为雇主地址
+  const employee = accounts[1]; //当前账号的第2个地址为雇员地址
+  const employee_null = 0x0; //空地址
+  const guest = accounts[5]; //非雇员地址，用于测试以非雇主身份添加雇员
+  const salary = 1; //雇员薪水
 
   it("Test call addEmployee() by owner", function () {
     var payroll;
@@ -15,6 +15,7 @@ contract('Payroll', function (accounts) {
     });
   });
 
+  //测试为雇员赋值负薪水
   it("Test call addEmployee() with negative salary", function () {
     var payroll;
     return Payroll.new().then(instance => {
@@ -25,6 +26,7 @@ contract('Payroll', function (accounts) {
     });
   });
 
+  //测试为雇员赋值零薪水
   it("Test call addEmployee() with zero salary", function () {
     var payroll;
     return Payroll.new().then(instance => {
@@ -35,6 +37,7 @@ contract('Payroll', function (accounts) {
     });
   });
 
+  //测试为以非雇主身份添加雇员
   it("Test addEmployee() by guest", function () {
     var payroll;
     return Payroll.new().then(function (instance) {
@@ -47,6 +50,7 @@ contract('Payroll', function (accounts) {
     });
   });
 
+  //测试添加空地址的雇员
   it("Test addEmployee() by 0x0 address", function () {
     var payroll;
     return Payroll.new().then(function (instance) {
@@ -59,6 +63,7 @@ contract('Payroll', function (accounts) {
     });
   });
 
+  //测试添加雇员失败的情况
   it("Test addEmployee() failed to add employee", function () {
     var payroll;
     return Payroll.new().then(function (instance) {
